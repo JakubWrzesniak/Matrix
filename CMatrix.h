0c470296a05f22711f410566903ab8418ce249ba
Jakub Wrzesniak <jakub.wrzesniak@outlook.com>
2021-01-05 15:54:27 +0100

    Dodaj pliki projektów.

---
+++ CMatrix.h
@@ -0,0 +1,270 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class CMatrix
+{
+public:
+	CMatrix();
+	CMatrix(int iMSize, int iNSize);
+	//CMatrix(CMatrix tMatrix);
+	~CMatrix();
+
+	void vPrintMatrix();
+	bool bCreateMatrix(int iMSize, int iNSize);
+
+	T tGetValuFrom(int iM, int iN);
+	bool bSetValueAt(int iM, int iN, T tNewValue);
+	bool bIsSquareMatrix() { return iMSize == iNSize; };
+	bool bSetIdentityMatrix();
+
+	int iGetIMSize() { return iMSize; };
+	int iGetINSize() { return iNSize; };
+
+	CMatrix<T>* transpose();
+
+	CMatrix<T>* operator+ (CMatrix<T> &tMatrix);
+	CMatrix<T>* operator- (CMatrix<T> &tMatrix);
+
+	CMatrix<T>* operator* (int sC);
+	template <typename S>
+	CMatrix<S>* operator* (S sC);
+
+	CMatrix<T>* operator* (CMatrix<int> &iMatrix);
+	template <typename S>
+	CMatrix<S>* operator* (CMatrix<S> &sMatrix);
+private:
+	T** tMatrix;
+	int iMSize = 5;
+	int iNSize = 5; 
+
+	static void vFillDefMatrix(T** tMatrix, int iMSize, int iNSize);
+};
+
+template<typename T>
+inline CMatrix<T>::CMatrix()
+{
+	tMatrix = new T* [iMSize];
+	for (int i = 0; i < iMSize; i++)
+		tMatrix[i] = new T[iNSize];
+	vFillDefMatrix();
+}
+
+template<typename T>
+inline CMatrix<T>::CMatrix(int iMSize, int iNSize)
+{
+	if (iMSize > 0 && iNSize > 0) {
+		this->iMSize = iMSize;
+		this->iNSize = iNSize;
+	}
+	else 
+		cout << "Wymiary macierzy musza byc wieksze od 0" << endl;
+	
+	tMatrix = new T *[iMSize];
+	
+	for (int i = 0; i < iMSize; i++)
+		tMatrix[i] = new T[iNSize];
+
+	vFillDefMatrix(tMatrix, iMSize, iNSize);
+}
+
+template<typename T>
+inline CMatrix<T>::~CMatrix()
+{
+	for (int i = 0; i < iMSize; i++) 
+		delete[] tMatrix[i];
+	delete tMatrix;
+	
+}
+
+template<typename T>
+inline void CMatrix<T>::vPrintMatrix()
+{
+	for (int i = 0; i < iMSize; i++) {
+		cout << "| ";
+		for (int j = 0; j < iNSize; j++)
+			cout << tMatrix[i][j] << " ";
+		cout << " |" << endl;
+	}
+}
+
+template<typename T>
+inline bool CMatrix<T>::bCreateMatrix(int iMSize, int iNSize)
+{
+	if (iMSize > 0 && iNSize > 0) {
+		T** tNewMatrix = new T *[iMSize];
+		
+		for (int i = 0; i < iMSize; i++)
+			tNewMatrix[i] = new T[iNSize];
+		vFillDefMatrix(tNewMatrix, iMSize, iNSize);
+		
+		for (int i = 0; i < (this->iMSize > iMSize ? iMSize : this->iMSize ); i++) {
+			for (int j = 0; j < (this->iNSize > iNSize ? iNSize : this->iNSize); j++) {
+				tNewMatrix[i][j] = tMatrix[i][j];
+			}
+		}
+		
+		for (int i = 0; i < this->iMSize; i++)
+			delete[] tMatrix[i];
+		delete[] tMatrix;
+
+		tMatrix = tNewMatrix;
+		this->iMSize = iMSize;
+		this->iNSize = iNSize;
+		return true;
+	}
+	else {
+		cout << "Wymiary macierzy musza byc wieksze od 0" << endl;
+		return false;
+	}
+}
+
+template<typename T>
+inline T CMatrix<T>::tGetValuFrom(int iM, int iN)
+{
+	if (iM < iMSize && iN < iNSize) {
+		return tMatrix[iM][iN];
+	}
+	else {
+		cout << "Wykroczono poza obszar tablicy" << endl;
+		return NULL;
+	}
+}
+
+template<typename T>
+inline bool CMatrix<T>::bSetValueAt(int iM, int iN, T tNewValue)
+{
+	if (iM < iMSize && iN < iNSize) {
+		tMatrix[iM][iN] = tNewValue;
+		return true;
+	}
+	else {
+		cout << "Wykroczono poza obszar tablicy" << endl;
+		return false;
+	}
+}
+
+template<typename T>
+inline bool CMatrix<T>::bSetIdentityMatrix()
+{
+	if (bIsSquareMatrix()) {
+		vFillDefMatrix(tMatrix, iMSize, iNSize);
+		for (int i = 0; i < iMSize; i++)
+			tMatrix[i][i] = 1;
+		return true;
+	}
+	else {
+		cout << "Macierz nie jest macierza kwadratowa." << endl;
+		return false;
+	}
+}
+
+template<typename T>
+inline CMatrix<T>* CMatrix<T>::transpose()
+{
+	CMatrix<T>* tNewMatrix = new CMatrix<T>(iNSize, iMSize);
+	for (int i = 0; i < iMSize; i++)
+		for (int j = 0; j < iNSize; j++)
+			tNewMatrix->bSetValueAt(j, i, tMatrix[i][j]);
+	return tNewMatrix;
+}
+
+template<typename T>
+inline CMatrix<T>* CMatrix<T>::operator+(CMatrix<T>& tMatrix)
+{
+	if (iMSize == tMatrix.iMSize && iNSize == tMatrix.iNSize) {
+		CMatrix<T>* tNewMatrix = new CMatrix<T>(iMSize, iNSize);
+		for (int i = 0; i < iMSize; i++)
+			for (int j = 0; j < iNSize; j++)
+				tNewMatrix->bSetValueAt(i, j, this->tMatrix[i][j] + tMatrix.tGetValuFrom(i, j));
+		return tNewMatrix;
+	}
+	return this;
+}
+
+template<typename T>
+inline CMatrix<T>* CMatrix<T>::operator-(CMatrix<T>& tMatrix)
+{
+	if (iMSize == tMatrix.iMSize && iNSize == tMatrix.iNSize) {
+		CMatrix<T>* tNewMatrix = new CMatrix<T>(iMSize, iNSize);
+		for (int i = 0; i < iMSize; i++)
+			for (int j = 0; j < iNSize; j++)
+				tNewMatrix->bSetValueAt(i, j, this->tMatrix[i][j] - tMatrix.tGetValuFrom(i, j));
+		return tNewMatrix;
+	}
+	return this;
+}
+
+template<typename T>
+inline CMatrix<T>* CMatrix<T>::operator*(int sC)
+{
+	CMatrix<T>* tNewMatrix = new CMatrix<T>(iMSize, iNSize);
+	for (int i = 0; i < iMSize; i++)
+		for (int j = 0; j < iNSize; j++)
+			tNewMatrix->bSetValueAt(i, j, tMatrix[i][j] * sC);
+	return tNewMatrix;
+}
+
+template<typename T>
+inline CMatrix<T>* CMatrix<T>::operator*(CMatrix<int> &iMatrix)
+{
+	if (iNSize == iMatrix.iGetIMSize()) {
+		CMatrix<T> *tNewMatrix = new CMatrix<T>(iMSize, iMatrix.iGetINSize());
+		for(int i = 0; i < iMSize ; i++)
+			for (int j = 0; j < iMatrix.iGetINSize(); j++) {
+				T s = 0;
+				for (int k = 0 ;k < iNSize; k++)
+					s += tMatrix[i][k] * iMatrix.tGetValuFrom(k,j);
+				tNewMatrix->bSetValueAt(i,j,s);
+			}
+		return tNewMatrix;
+	}
+	else {
+		cout << "Nie mozna pomnozyc podanych macierzy." << endl;
+		return new CMatrix<T>(iMSize, iNSize);
+	}
+}
+
+template<typename T>
+template<typename S>
+inline CMatrix<S>* CMatrix<T>::operator*(S sC)
+{
+	CMatrix<S>* tNewMatrix = new CMatrix<S>(iMSize, iNSize);
+	for (int i = 0; i < iMSize; i++)
+		for (int j = 0; j < iNSize; j++)
+			tNewMatrix->bSetValueAt(i, j, tMatrix[i][j] * sC);
+	return tNewMatrix;
+}
+
+template<typename T>
+template<typename S>
+inline CMatrix<S>* CMatrix<T>::operator*(CMatrix<S> &sMatrix)
+{
+	CMatrix<S>* tNewMatrix = new CMatrix<S>(iMSize, sMatrix.iGetINSize());
+	if (iNSize == sMatrix.iGetIMSize()) {
+		for (int i = 0; i < iMSize; i++)
+			for (int j = 0; j < sMatrix.iGetINSize(); j++) {
+				S s = 0;
+				for (int k = 0; k < iNSize; k++)
+					s += tMatrix[i][k] * sMatrix.tGetValuFrom(k, j);
+				tNewMatrix->bSetValueAt(i, j, s);
+			}
+	}
+	else {
+		cout << "Nie mozna pomnozyc podanych macierzy." << endl;
+	}
+	return tNewMatrix;
+}
+
+template<typename T>
+inline void CMatrix<T>::vFillDefMatrix(T** tMatrix, int iMSize, int iNSize)
+{
+	for (int i = 0; i < iMSize; i++) {
+		for (int j = 0; j < iNSize; j++)
+			tMatrix[i][j] = NULL;
+	}
+}
+
+
---
+++ Źródło.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "CMatrix.h"
+
+using namespace std;
+
+void MatrixTest() {
+	CMatrix<int>* matrix1 = new CMatrix<int>(2, 4);
+	CMatrix<int>* matrix2 = new CMatrix<int>(3, 3);
+	CMatrix<int>* matrix3;
+	CMatrix<double>* matrix4;
+	CMatrix<double>* matrix5 = new CMatrix<double>(2, 3);
+	CMatrix<float>* matrix6 = new CMatrix<float>(3, 4);
+	CMatrix<int>* vector1 = new CMatrix<int>(1, 3);
+	CMatrix<int>* vector2 = new CMatrix<int>(3, 1);
+
+	matrix1->vPrintMatrix();
+	matrix1->bSetValueAt(0, 0, 1);
+	matrix1->bSetValueAt(1, 0, 1);
+	matrix1->bSetValueAt(1, 1, 1);
+	matrix1->bSetValueAt(0, 3, 5);
+	matrix1->vPrintMatrix();
+	matrix1->bCreateMatrix(3, 3);
+	matrix1->vPrintMatrix();
+	cout << matrix1->tGetValuFrom(0, 0) << endl;;
+	matrix1->bSetIdentityMatrix();
+	matrix1->vPrintMatrix();
+
+	matrix2->bSetValueAt(0,0,4);
+	matrix2->bSetValueAt(1,0,4);
+	matrix2->bSetValueAt(2,0,4);
+	matrix2->bSetValueAt(0,1,4);
+	matrix2->bSetValueAt(1,1,4);
+	matrix2->bSetValueAt(2,1,4);
+	matrix2->bSetValueAt(0,2,4);
+	matrix2->bSetValueAt(1,2,4);
+	matrix2->bSetValueAt(2,2,4);
+
+	matrix5->bSetValueAt(0,0,2.5);
+	matrix5->bSetValueAt(1,0,2.5);
+	matrix5->bSetValueAt(0,1,2.5);
+	matrix5->bSetValueAt(1,1,2.5);
+	matrix5->bSetValueAt(0,2,2.5);
+	matrix5->bSetValueAt(1,2,2.5);
+
+	matrix6->bSetValueAt(0,0,1);
+	matrix6->bSetValueAt(0,1,2);
+	matrix6->bSetValueAt(0,2,3);
+	matrix6->bSetValueAt(0,3,4);
+	matrix6->bSetValueAt(1,0,5);
+	matrix6->bSetValueAt(1,1,6);
+	matrix6->bSetValueAt(1,2,7);
+	matrix6->bSetValueAt(1,3,8);
+	matrix6->bSetValueAt(2,0,9);
+	matrix6->bSetValueAt(2,1,10);
+	matrix6->bSetValueAt(2,2,11);
+	matrix6->bSetValueAt(2,3,12);
+
+	vector1->bSetValueAt(0, 0, 1);
+	vector1->bSetValueAt(0, 1, 3);
+	vector1->bSetValueAt(0, 2, -5);
+
+	vector2->bSetValueAt(0, 0, 4);
+	vector2->bSetValueAt(1, 0, -2);
+	vector2->bSetValueAt(2, 0, -1);
+
+	matrix2->vPrintMatrix();
+	matrix3 = *matrix1 + *matrix2;
+	matrix3->vPrintMatrix();
+	matrix3 = *matrix3 - *matrix2;
+	matrix3->vPrintMatrix();
+	matrix4 = *matrix3 * 4.5;
+	matrix4->vPrintMatrix();
+	matrix3 = *matrix3 * 4;
+	matrix3->vPrintMatrix();
+	matrix4 = *matrix4 * 3;
+	matrix4->vPrintMatrix();
+	matrix5->vPrintMatrix();
+	matrix6->vPrintMatrix();
+	(*matrix5 * *matrix6)->vPrintMatrix();
+
+	(*vector1 * *vector2)->vPrintMatrix();
+	matrix6->transpose()->vPrintMatrix();
+}
+
+int main(int argc, char* argv[]) {
+	MatrixTest();
+	return 0;
+}
+
